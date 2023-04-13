8a8ec1f82e217e90b3c0b5e1f3be9e947229e387
Pedro <pedro.gongora@egebound.net>
2023-04-13 17:16:03 -0600

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -17,9 +17,9 @@ self.addEventListener("install", function (event) {
 self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
-      if (response) {
+      /* if (response) {
         return response;
-      }
+      } */
       return fetch(event.request);
     })
   );
